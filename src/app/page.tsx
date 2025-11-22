1d4151111ae748566304a0c7c7947f2146326d42
Eva Frankovic <emfrankovic7@gmail.com>
2025-11-21 19:08:31 -0500

    Added more styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,17 +23,17 @@ export default function HomePage() {
           />
         </div>
         <h1 className="text-5xl font-bold mb-4">Eva Frankovic</h1>
-        <p className="text-center mb-6 mt-2 ml-10 mr-10"> I am currently a User Engagement Specialist at the Pittsburgh Supercomputing Center, part of Carnegie Mellon University. Where alongside of adminsitrative work, I assist in. the development 
+        <p className="text-center mb-6 mt-2 ml-10 mr-10"> I am currently a User Engagement Specialist at the Pittsburgh Supercomputing Center, part of Carnegie Mellon University. Where alongside of adminsitrative work, I assist in the development 
           of there internal web application using Ruby and Ruby on Rails. Recently, we have began tranisitioning the application to using React and Tailwind.css. I graduated from Bucknell University in May 2024 with a Bachelor’s degree in Computer Science.
           While at Bucknell, I competed on the university’s varsity soccer team, where we won two Patriot League Championships. Now, as a recent graduate, I’m continuing to grow my technical toolkit by exploring frameworks such
           as Bootstrap and React while strengthening my skills in HTML, CSS, and JavaScript. </p>
         <div className="flex gap-6 justify-center mt-2">
-          <FontAwesomeIcon icon={faLinkedin} className="w-13 h-13 text-gray-700 hover:text-blue-800 transition" />
-          <FontAwesomeIcon icon={faGithub} className="w-13 h-13 text-gray-700 hover:text-blue-800 transition" />
+          <FontAwesomeIcon icon={faLinkedin} className="icon" />
+          <FontAwesomeIcon icon={faGithub} className="icon" />
         </div>
 
         <Button className="m-8" asChild>
-          <a href="/contact"> Contact </a>
+          <a className="button" href="/contact"> Contact </a>
         </Button>
       </section>
     </>
